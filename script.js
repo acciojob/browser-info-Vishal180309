639bcb45a7db9c1cd9d6f5f445e860a77d044150
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-02-27 11:11:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const browserInfoDiv = document.getElementById("browser-info");
+
+const userAgent = navigator.userAgent;
+const browserName = navigator.appName;
+const version = navigator.appVersion;
+
+let browserNameString = "";
+if (browserName === "Netscape") {
+    browserNameString = "Firefox";
+} else {
+    browserNameString = browserName;
+}
+
+browserInfoDiv.innerText = `You are using ${browserNameString} version ${version}`;
